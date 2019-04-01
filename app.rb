40c9ef324746c8074dabf2ffa32154ae15cb0c3b
bbennett7 <bryn.bennett@gmail.com>
2019-04-01 10:01:04 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Bryn Bennett"
+  end
+
+  get '/hometown' do
+    "My hometown is Los Angeles, CA"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Embryonic Journey' by Jefferson Airplane"
+  end
+
 end
