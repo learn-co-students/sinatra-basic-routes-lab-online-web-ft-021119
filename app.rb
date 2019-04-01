2379307f9d6c29d8dd796f956186dcd06c21fa19
Minajeanne <albrown07@mac.com>
2019-04-01 14:20:51 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Alicia"
+  end
+
+  get '/hometown' do
+    "My hometown is Poughkeepsie"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is When The Levee Breaks by Led Zeppelin"
+  end
 end
