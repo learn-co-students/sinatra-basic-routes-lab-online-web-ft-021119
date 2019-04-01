83fdd8b3288766248c8dab5230cd5cfa8f81470f
kirstenwerner <kirstenmwerner@gmail.com>
2019-04-01 15:43:44 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Kirsten"
+  end
+
+  get '/hometown' do
+    "My hometown is Seattle, WA"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 3rd Planet by Modest Mouse"
+  end
+
 end
